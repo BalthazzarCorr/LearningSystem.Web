d73311fbda65c6010000196983f8a69f0df9f7c5
BalthazzarCorr <peev.petar@gmail.com>
2017-11-30 23:51:17 +0200

    fixed bugs

--- LearningSystem.Web/Models/AccountViewModels/LoginViewModel.cs
+++ LearningSystem.Web/Models/AccountViewModels/LoginViewModel.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LearningSystem.Web.Models.AccountViewModels
+﻿namespace LearningSystem.Web.Models.AccountViewModels
 {
-    public class LoginViewModel
+   using System.ComponentModel.DataAnnotations;
+
+
+   public class LoginViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
--- LearningSystem.Web/Models/AccountViewModels/RegisterViewModel.cs
+++ LearningSystem.Web/Models/AccountViewModels/RegisterViewModel.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LearningSystem.Web.Models.AccountViewModels
+﻿namespace LearningSystem.Web.Models.AccountViewModels
 {
-    public class RegisterViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+   using System;
+   using System.ComponentModel.DataAnnotations;
+   using Data;
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+   public class RegisterViewModel
+   {
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
+      [Required]
+      [StringLength(50)]
+      public string Username { get; set; }
+
+      [Required]
+      [MinLength(DataConstants.UserNameMinLenght)]
+      [MaxLength(DataConstants.UserNameMaxLenght)]
+      public string Name { get; set; }
+
+      [Required]
+      [EmailAddress]
+      [Display(Name = "Email")]
+      public string Email { get; set; }
+
+      [Required]
+      [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+      [DataType(DataType.Password)]
+      [Display(Name = "Password")]
+      public string Password { get; set; }
+
+      [DataType(DataType.Password)]
+      [Display(Name = "Confirm password")]
+      [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+      public string ConfirmPassword { get; set; }
+
+      [DataType(DataType.Date)]
+      public DateTime Birthdate { get; set; }
+   }
 }
