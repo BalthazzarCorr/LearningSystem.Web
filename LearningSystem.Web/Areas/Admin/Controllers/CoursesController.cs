afbb951e9257887df86c448cae33f36747be2e15
BalthazzarCorr <peev.petar@gmail.com>
2017-12-03 02:10:09 +0200

    new

--- LearningSystem.Web/Areas/Admin/Controllers/CoursesController.cs
+++ LearningSystem.Web/Areas/Admin/Controllers/CoursesController.cs
@@ -1,11 +1,37 @@
 ﻿namespace LearningSystem.Web.Areas.Admin.Controllers
 {
+   using System.Linq;
+   using System.Threading.Tasks;
+   using Data.Models;
+   using Microsoft.AspNetCore.Identity;
    using Microsoft.AspNetCore.Mvc;
+   using Microsoft.AspNetCore.Mvc.Rendering;
+   using Models.Courses;
 
    public class CoursesController : BaseAdminController
    {
+      private readonly UserManager<User> _userManager;
 
-      public IActionResult Create;
+      public CoursesController(UserManager<User> userManager)
+      {
+         this._userManager = userManager;
+      }
+
+
+      public async Task<IActionResult> Create()
+      {
+         var trainers = await this._userManager.GetUsersInRoleAsync(WebConstants.TrainerRole);
+         var trainerListItems = trainers.Select(t => new SelectListItem
+         {
+            Value = t.Id,
+            Text = t.UserName
+         }).ToList();
+
+         return View(new AddCourseFromModel
+         {
+            Trainers = trainerListItems
+         });
+      }  
 
    }
 }
---
+++ LearningSystem.Web/Areas/Admin/Models/Courses/AddCourseFromModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningSystem.Web.Areas.Admin.Models.Courses
+{
+   using System.Collections.Generic;
+   using Microsoft.AspNetCore.Mvc.Rendering;
+
+   public class AddCourseFromModel
+    {
+       public IEnumerable<SelectListItem>Trainers { get; set; }
+    }
+}
